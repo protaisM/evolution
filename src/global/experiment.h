d9515e0dccbaa34239aefe71d3640f4975c3a54c
protaisM <m.protais@proton.me>
2026-06-30 00:09:35 +0200

    Clean up experiment

--- src/global/experiment.h
+++ src/global/experiment.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "display_parameter.h"
 #include "experiment_display.h"
 #include "experiment_parameter.h"
 #include "experiment_rules.h"
@@ -9,20 +8,9 @@
 #include "logger.h"
 #include "map.h"
 #include "position.h"
-#include "predator.h"
 
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/Font.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/Text.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window/Event.hpp>
-#include <array>
 #include <cstring>
-#include <iostream>
 #include <memory>
-#include <string>
 
 class ExperimentController;
 
@@ -31,13 +19,9 @@ template <typename Player> class IExperiment {};
 template <typename Mouse> class Experiment : public IExperiment<Mouse> {
 private:
   Logger *const m_logger;
-
   std::unique_ptr<ExperimentRules<Mouse>> m_experiment_rules;
   Level m_level;
-
   std::vector<Mouse> m_mice;
-
-  // parameters
   ExperimentParameters m_params;
 
   friend class ExperimentController;
@@ -46,24 +30,18 @@ private:
 public:
   Experiment(Logger *log, Map *map, ExperimentParameters const &params)
       : m_logger(log), m_level(map, 2), m_params(params) {
-
-    // reproduction strategy
     m_experiment_rules = std::make_unique<FitnessFunction<Mouse>>();
-
-    // mice
     for (unsigned int i = 0; i < m_params.maximal_mice_number; i++) {
       m_mice.emplace_back(map);
-      if (!m_params.randomized_spawning_point) {
-        m_mice[i].set_position(m_params.spawning_point);
-        m_mice[i].set_angle(m_params.spawning_angle);
-      }
     }
+    start_of_the_round();
   }
 
   ~Experiment() = default;
 
   void do_one_step() {
     m_params.time = m_params.time + m_params.dt;
+
     std::vector<PositionAngle> predators_states;
     for (std::unique_ptr<Predator> const &predator : m_level.m_predators) {
       predator->do_one_step(m_params.dt);
@@ -92,25 +70,9 @@ public:
     if (!m_experiment_rules->condition_end_generation(m_params)) {
       return;
     }
-    double avg_fitness = 0;
-    double max_fitness = 0;
-    for (Mouse const &mouse : m_mice) {
-      avg_fitness += mouse.get_fitness() / m_params.maximal_mice_number;
-      if (mouse.get_fitness() > max_fitness) {
-        max_fitness = mouse.get_fitness();
-      }
-    }
-    m_logger->store(
-        m_params.generation, m_params.time / m_params.generation_duration,
-        (double)m_params.nb_alive_mice / (double)m_params.maximal_mice_number,
-        max_fitness);
+    send_to_logger();
     m_experiment_rules->reproduce(m_mice, m_params);
-    for (Mouse &mouse : m_mice) {
-      mouse.start_of_round();
-    }
-    for (std::unique_ptr<Predator> const &predator : m_level.m_predators) {
-      predator->start_of_the_round();
-    }
+    start_of_the_round();
   }
 
   void add_to_dt(double dt) { m_params.dt = std::max(0.005, m_params.dt + dt); }
@@ -123,5 +85,34 @@ public:
   void add_to_minimal_mice_number(double dn) {
     m_params.minimal_mice_number =
         std::max(10., m_params.minimal_mice_number + dn);
+  }
+
+private:
+  void start_of_the_round() {
+    for (Mouse &mouse : m_mice) {
+      mouse.start_of_round();
+      if (!m_params.randomized_spawning_point) {
+        mouse.set_position(m_params.spawning_point);
+        mouse.set_angle(m_params.spawning_angle);
+      }
+    }
+    for (std::unique_ptr<Predator> const &predator : m_level.m_predators) {
+      predator->start_of_the_round();
+    }
+  }
+
+  void send_to_logger() {
+    double avg_fitness = 0;
+    double max_fitness = 0;
+    for (Mouse const &mouse : m_mice) {
+      avg_fitness += mouse.get_fitness() / m_params.maximal_mice_number;
+      if (mouse.get_fitness() > max_fitness) {
+        max_fitness = mouse.get_fitness();
+      }
+    }
+    m_logger->store(
+        m_params.generation, m_params.time / m_params.generation_duration,
+        (double)m_params.nb_alive_mice / (double)m_params.maximal_mice_number,
+        max_fitness);
   }
 };
